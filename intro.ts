3ba33ba11a9f92f3f4e37bbd75d39d890e1ede4a
Fatma Nur Kılıçkaya <82950952+fatmanurkilickaya@users.noreply.github.com>
2024-04-15 15:58:44 +0300

    Add files via upload

---
+++ intro.ts
@@ -0,0 +1,20 @@
+function selamVer(isim:string){
+    return "Merhaba " + isim;
+}
+
+selamVer("nur");
+
+//var sehir = "Ankara";
+let sehir:string = "Ankara";
+let sayi:number = 10;
+sayi = 4;
+let _bool :boolean = false;
+if(sayi >10){
+    _bool = true
+}
+
+let sayilar:number[] = [1,2,3,4];
+let sayilar2: Array<number>;
+let dizi:[number, string, boolean]
+ = [1,"metin",true]
+
